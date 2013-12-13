ca40da7fe098746ef0684bb08f3f979c5466fbf5
Robinson Tryon <qubit@runcibility.com>
2013-12-13 03:43:32 -0500

    mimetypes: Add flags and capability to save attachments

--- stats/mimetypes/mimetypestats.py
+++ stats/mimetypes/mimetypestats.py
@@ -29,6 +29,8 @@ import stat
 import sys
 import time
 import xmlrpclib
+import pprint
+
 try:
     from urllib.request import urlopen
 except:
@@ -39,6 +41,15 @@ except:
     import xmlrpclib
 from xml.dom import minidom
 from xml.sax.saxutils import escape
+
+# If we want lots of information during the run...
+VERBOSE = 0
+# If we want to log our results.
+LOG_RESULTS = 0
+# The maximum # of bugs we'll iterate over at one time.
+MAX_NUM_BUGS = 1000
+# Download all attachments to disk.
+DOWNLOAD_ATTACHMENTS = 0
 
 def urlopen_retry(url):
     maxretries = 3
@@ -51,15 +62,17 @@ def urlopen_retry(url):
                 raise
             print("retrying...")
 
+# DEPRECATED: Using get_through_rpc_query now
 def get_from_bug_url_via_xml(url, mimetype):
     id = url.rsplit('=', 2)[1]
-    print("parsing " + id)
+    if VERBOSE: print("Parsing Bug ID: " + id)
     sock = urlopen_retry(url+"&ctype=xml")
     dom = minidom.parse(sock)
     sock.close()
     count = 0
     for attachment in dom.getElementsByTagName('attachment'):
-        #print(" mimetype is", end=' ')
+        if VERBOSE: print("--> Attachment: " + attachment)
+        if VERBOSE: print("--> mimetype is", end=' ')
         for node in attachment.childNodes:
             if node.nodeName == 'type':
                 #print(node.firstChild.nodeValue, end=' ')
@@ -71,51 +84,94 @@ def get_from_bug_url_via_xml(url, mimetype):
                     break
     return count
 
+# Get information about the attachment and its mimetype.
 def get_through_rpc_query(url, mimetype):
+    # We're interested in this bug because of one of its attachments.
+    if VERBOSE: print("Bug URL: " + url)
     id = url.rsplit('=', 2)[1]
-    print(id)
+    if VERBOSE: print("Bug ID: " + id)
     query = dict()
     query['ids'] = id
     proxy = xmlrpclib.ServerProxy('https://bugs.freedesktop.org/xmlrpc.cgi')
     result = proxy.Bug.attachments(query)
-    bugs = result['bugs'][id]
+    attachments = result['bugs'][id]
     count = 0
-#    import pprint
-#    pp = pprint.PrettyPrinter(indent=4)
-#    pp.pprint(bugs)
 
-    for bug in bugs:
-        if bug['content_type'] == mimetype:
+    if VERBOSE:
+        # Print out structured data for our attachments.
+        pp = pprint.PrettyPrinter(indent=4)
+        pp.pprint(attachments)
+
+    for a in attachments:
+        a_id = str(a['id'])
+        a_file_name = str(a['file_name'])
+        if VERBOSE:
+            print("--> Attachment ID  : " + a_id)
+            print("--> Attachment Name: " + a_file_name)
+            print("--> Attachment MIME: " + str(a['content_type']))
+
+        if DOWNLOAD_ATTACHMENTS:
+            # Grab the attachment.
+            # The new filename is <bug_id>_<attachment_id>_<filename>.
+            filepath = "attachments/%s_%s_%s" % (a_id, id, a_file_name)
+            if VERBOSE:
+                print("----> Writing attachment out to %s." % filepath)
+                with open("attachments/%s" % filepath, "wb") as handle:
+                    handle.write(a['data'].data)
+
+        if a['content_type'] == mimetype:
             count += 1
 
     return count
 
 def get_through_rss_query(queryurl, mimetype):
     url = queryurl + '?query_format=advanced&f1=attachments.mimetype&v1=application%2Foctet-stream&o1=equals&product=LibreOffice&ctype=atom'
-    print('url is ' + url)
+    print('Query url is: ' + url)
     d = feedparser.parse(url)
-    print(str(len(d['entries'])) + ' bugs to process')
+    
+    total_bugs = len(d['entries'])
+    print('We have %s bugs to process.' % total_bugs)
+    if (total_bugs > MAX_NUM_BUGS):
+        print("\nWARNING: total_bugs(%s) is larger than MAX_NUM_BUGS(%s).\n" %
+              (total_bugs, MAX_NUM_BUGS))
+
+    print("----------------------------")
     attachCount = 0
-    for entry in d['entries']:
+    numBugs = 0
+    for bug in d['entries']:
+        # Break-out when we've reached our bug limit
+        if(MAX_NUM_BUGS > numBugs):
+            numBugs += 1
+        else:
+            break
+
         try:
-            attachCount = attachCount + get_through_rpc_query(entry['id'], mimetype)
+            print ("before")
+            attachCount = attachCount + get_through_rpc_query(bug['id'],
+                                                              mimetype)
+            print("After")
         except KeyboardInterrupt:
             raise # Ctrl+C should work
         except:
-            print(entry['id'] + " failed: " + str(sys.exc_info()[0]))
+            print(bug['id'] + " failed: " + str(sys.exc_info()[0]))
             pass
 
-        print("Total count = " + str(attachCount))
+        if VERBOSE: print("Total count = " + str(attachCount))
 
-    #write it to a log
-    file = open("mimetypecount.csv", "a")
-    file.write("\"" + time.strftime("%d/%m/%Y") + "\",\"" + str(attachCount) + "\"\n")
-    file.close()
+    # Write it to a log.
+    if LOG_RESULTS:
+        file = open("mimetypecount.csv", "a")
+        file.write("\"" + time.strftime("%d/%m/%Y") + "\",\"" +
+                   str(attachCount) + "\"\n")
+        file.close()
 
+print("Starting our search!")
 
 rss_bugzilla = 'http://bugs.libreoffice.org/buglist.cgi'
 mimetype = 'application/octet-stream'
 
 get_through_rss_query(rss_bugzilla, mimetype)
+
+print("Our search has come to an end...")
 
 # vim:set shiftwidth=4 softtabstop=4 expandtab:
