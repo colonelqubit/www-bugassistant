8645361bc2f8525ac5630b9bd5f4625c32ab6bf4
Robinson Tryon <qubit@runcibility.com>
2013-12-16 04:50:17 -0800

    mimetypes: Add support for HTML/data table export

--- stats/mimetypes/mimetypestats.py
+++ stats/mimetypes/mimetypestats.py
@@ -50,6 +50,91 @@ LOG_RESULTS = 0
 MAX_NUM_BUGS = 1000
 # Download all attachments to disk.
 DOWNLOAD_ATTACHMENTS = 0
+# Print an HTML file containing detailed information about the
+# scrutinized attachments.
+CREATE_HTML_REPORT = 0
+
+# Some static variables relating to our Bugzilla and tests.
+BUGZILLA_URL = "https://bugs.libreoffice.org/"
+RSS_BUGZILLA = BUGZILLA_URL + "buglist.cgi"
+ATTACHMENT_URL = "https://bugs.libreoffice.org/attachment.cgi?id="
+MIMETYPE = 'application/octet-stream'
+
+# This function takes a hash (keyed by bug_id) containing information
+# about bugs, attachments, their mimetype, etc.., and returns an HTML
+# table displaying information about them with links, etc..
+def attachment_info_to_html_table(bugDict):
+    # We seem to want enough tweaks to the headers that it doesn't
+    # make sense to try to dynamically guess/format them.
+    attachmentHeaders = {'id'        : [0, 'Attachment ID'],
+                         'edit'      : [1, 'Edit this attachment'],
+                         'filename'  : [2, 'File Name'],
+                         'extension' : [3, 'Filename Extension'],
+                         'filepath'  : [4, 'File Path'],
+                         'mimetype'  : [5, 'MIME-type'],
+                         'lastChange': [6, 'Last Time Changed in DB']}
+    # Sort our header keys based on our index value
+    headerKeys = sorted(attachmentHeaders.keys(),
+                        key=lambda k: attachmentHeaders[k][0])
+    print("<table>\n")
+    print("<tr><th>Bug ID</th>\n")
+    for k in headerKeys:
+        print("  <th>{0}</th>\n".format(attachmentHeaders[k][1]))
+    print ("</tr>\n")
+
+    # Print our header row.
+    for bug_id, attachments in bugDict.items():
+
+        # Print out a row for each bug.
+        print("<tr><td><a href=\"{0}{1}\">{1}</a></td></tr>\n".format(BUGZILLA_URL, bug_id, bug_id))
+    
+        for attachment_id, variables in attachments.items():
+            # Print out a row for each attachment.
+            print("<tr><td></td>\n")
+            for k in headerKeys:
+                # Provide a link into Bugzilla for the attachment.
+                if(k == "id"):
+                    print("  <td><a href=\"{0}{1}\">{1}</a></td>\n".format(ATTACHMENT_URL, variables[k]))
+                # Also provide a link for editing.
+                elif(k == "edit"):
+                    print("  <td><a href=\"{0}{1}&action=edit\">{1}</a></td>\n".format(ATTACHMENT_URL, variables["id"]))
+                else:
+                    print("  <td>{0}</td>\n".format(variables[k]))
+            print("</tr>\n")
+    print("</table>\n")
+
+# This function prints out an HTML page containing information about
+# all of the bugs/attachments we've scrutinized.
+def create_html_report(bugDict):
+    global BUGZILLA_URL
+
+    headString = """
+<html>
+  <head>
+    <title>The Glorious Battle against Incorrect MIME-types!</title>
+
+    <link rel="stylesheet" type="text/css" href="styles.css">
+  </head>
+
+  <body>
+    <h2>Information about the MIME-types of our <a href="%s">Bugzilla</a> attachments</h2>
+    """
+
+    tailString = """
+  </body>
+</html>
+    """
+    
+    print("Creating HTML file with attachment info.")
+    with open("index.html", "w") as handle:
+        # Redirect stdout into our file handle to make our statements
+        # much easier.
+        sys.stdout = handle
+        print(headString % BUGZILLA_URL)
+        attachment_info_to_html_table(bugDict) 
+        print(tailString)
+        # Fix stdout back to what it was, originally.
+        sys.stdout = sys.__stdout__
 
 def urlopen_retry(url):
     maxretries = 3
@@ -85,35 +170,45 @@ def get_from_bug_url_via_xml(url, mimetype):
     return count
 
 # Get information about the attachment and its mimetype.
-def get_through_rpc_query(url, mimetype):
+def get_through_rpc_query(url, mimetype, bugDict):
     # We're interested in this bug because of one of its attachments.
     if VERBOSE: print("Bug URL: " + url)
-    id = url.rsplit('=', 2)[1]
-    if VERBOSE: print("Bug ID: " + id)
+    bug_id = str(url.rsplit('=', 2)[1])
+    if VERBOSE: print("Bug ID: " + bug_id)
+
+    # Create a dict to store info about the attachments on this bug.
+    attachInfoDict = {}
+
     query = dict()
-    query['ids'] = id
-    proxy = xmlrpclib.ServerProxy('https://bugs.freedesktop.org/xmlrpc.cgi')
+    query['ids'] = bug_id
+    proxy = xmlrpclib.ServerProxy('https://bugs.freedesktop.org/xmlrpc.cgi',
+                                  use_datetime = True)
     result = proxy.Bug.attachments(query)
-    attachments = result['bugs'][id]
+    attachments = result['bugs'][bug_id]
     count = 0
 
     if VERBOSE:
         # Print out structured data for our attachments.
+        print("Structured Data for these attachments:")
         pp = pprint.PrettyPrinter(indent=4)
         pp.pprint(attachments)
 
     for a in attachments:
         a_id = str(a['id'])
         a_file_name = str(a['file_name'])
+        a_extension = os.path.splitext(a_file_name)[1][1:].strip()
+        # The new filename is <bug_id>_<attachment_id>_<filename>.
+        filepath = "attachments/%s_%s_%s" % (a_id, bug_id, a_file_name)
+
+        a_mime = str(a['content_type'])
+
         if VERBOSE:
             print("--> Attachment ID  : " + a_id)
             print("--> Attachment Name: " + a_file_name)
-            print("--> Attachment MIME: " + str(a['content_type']))
+            print("--> Attachment MIME: " + a_mime)
 
         if DOWNLOAD_ATTACHMENTS:
             # Grab the attachment.
-            # The new filename is <bug_id>_<attachment_id>_<filename>.
-            filepath = "attachments/%s_%s_%s" % (a_id, id, a_file_name)
             if VERBOSE:
                 print("----> Writing attachment out to %s." % filepath)
                 with open("attachments/%s" % filepath, "wb") as handle:
@@ -122,12 +217,30 @@ def get_through_rpc_query(url, mimetype):
         if a['content_type'] == mimetype:
             count += 1
 
+        # Store information about each attachment.
+        lastChangeTime = a['last_change_time'].strftime("%Y-%m-%d %H:%M")
+        attachInfoDict[a_id] = {'id'         : a_id,
+                                'filename'   : a_file_name,
+                                'extension'  : a_extension,
+                                'filepath'   : filepath,
+                                'mimetype'   : a_mime,
+                                'lastChange' : lastChangeTime
+                                # We'd also like to include last_updated date,
+                                # Info about file extension, etc..
+                                }
+
+    # Store the attachment dict back into our master dict.
+    bugDict[bug_id] = attachInfoDict
     return count
 
 def get_through_rss_query(queryurl, mimetype):
-    url = queryurl + '?query_format=advanced&f1=attachments.mimetype&v1=application%2Foctet-stream&o1=equals&product=LibreOffice&ctype=atom'
+    url = queryurl + '?query_format=advanced&f1=attachments.mimetype&v1=%s&o1=equals&product=LibreOffice&ctype=atom' % mimetype
     print('Query url is: ' + url)
     d = feedparser.parse(url)
+
+    # Create a dictionary var to store information about our
+    # bugs/attachments.
+    bugDict = {}
     
     total_bugs = len(d['entries'])
     print('We have %s bugs to process.' % total_bugs)
@@ -146,10 +259,9 @@ def get_through_rss_query(queryurl, mimetype):
             break
 
         try:
-            print ("before")
             attachCount = attachCount + get_through_rpc_query(bug['id'],
-                                                              mimetype)
-            print("After")
+                                                              mimetype,
+                                                              bugDict)
         except KeyboardInterrupt:
             raise # Ctrl+C should work
         except:
@@ -165,12 +277,14 @@ def get_through_rss_query(queryurl, mimetype):
                    str(attachCount) + "\"\n")
         file.close()
 
+    # Write it to an HTML file.
+    if CREATE_HTML_REPORT:
+        create_html_report(bugDict)
+
 print("Starting our search!")
 
-rss_bugzilla = 'http://bugs.libreoffice.org/buglist.cgi'
-mimetype = 'application/octet-stream'
 
-get_through_rss_query(rss_bugzilla, mimetype)
+get_through_rss_query(RSS_BUGZILLA, MIMETYPE)
 
 print("Our search has come to an end...")
 
